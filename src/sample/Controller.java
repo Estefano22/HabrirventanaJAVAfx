00a7347399f7d076f998f3b36ba45d126c8a805b
Estefano22 <sandulescustefano8@gmail.com>
2021-04-29 11:17:55 +0200

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,34 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Controller {
+
+    @FXML
+    public void Buttonventana1() {
+        try {
+            Stage stage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Ventana1.fxml"));
+            Pane root = (AnchorPane) loader.load();
+            Scene scene = new Scene(root, 650, 500);
+            stage.setResizable(false);
+            stage.setScene(scene);
+
+            ControllerVentana1 controller = loader.getController();
+            controller.ponerNumero(1);
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
---
+++ src/sample/ControllerVentana1.java
@@ -0,0 +1,41 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class ControllerVentana1 {
+
+    @FXML
+    private Button CerrarVentana;
+    @FXML
+    private Button CerrarApp;
+
+    @FXML
+    Label Contador;
+
+    @FXML
+    private void onButtonCerrarVentanaClicked() {
+        Stage stage = (Stage) CerrarVentana.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    private void onButtonCerrarAppClicked() {
+        System.exit(0);
+    }
+
+
+    public void ponerNumero(int numero) {
+        System.out.println(numero);
+
+    }
+
+
+    public void ContadorLabel(){
+
+
+    }
+
+}
